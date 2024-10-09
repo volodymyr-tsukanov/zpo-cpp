c8a61185e461c66435f6183f8dd030557930983d
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2024-10-09 13:21:04 +0200

    updated main (presentation structure)

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -31,71 +31,86 @@ T* findMin(T* tab[], int n){
 
 int main()
 {
-    int n = 6;
-    /*1
-    Adding<int> a1(5);
-    a1.add(6);
-    a1.show();
+    int zadN = 1, n = 6;
+    printf("n?: ");
+    cin >> zadN;
 
-    Adding<string> a2("Pro");
-    a2.add("gramowanie");
-    a2.show();*/
-
-    /*2
-    Student<float> s(5,"Ala");
-    s.showMark();
-    Student<int> s1(5,"Ala");
-    s1.showMark();
-    Student<string> s2(5,"Ala");
-    s2.showMark();*/
-
-    /*3
-    Adding<int>* arrAdd[3];
-    for(int i=0;i<3;i++)
+    switch(zadN){
+    case 1:
     {
-        arrAdd[i]=new Adding<int>(i);
+        /*1
+        Adding<int> a1(5);
+        a1.add(6);
+        a1.show();
+
+        Adding<string> a2("Pro");
+        a2.add("gramowanie");
+        a2.show();*/
+
+        /*2
+        Student<float> s(5,"Ala");
+        s.showMark();
+        Student<int> s1(5,"Ala");
+        s1.showMark();
+        Student<string> s2(5,"Ala");
+        s2.showMark();*/
+
+        /*3
+        Adding<int>* arrAdd[3];
+        for(int i=0;i<3;i++)
+        {
+            arrAdd[i]=new Adding<int>(i);
+        }
+        showAll(arrAdd,3);
+
+        Student<string>* arrStu[2];
+        for(int i=0;i<3;i++){
+            arrStu[i]=new Student<string>(i+2,"Ala");
+        }
+        showAll(arrStu,3);
+
+        for(int i=0; i<3; i++)
+            delete arrAdd[i];
+
+        for(int i=0; i<2; i++)
+            delete arrStu[i];*/
+
+        /*4
+        checkType<int>(3);*/
     }
-    showAll(arrAdd,3);
+    break;
+    case 2:
+    {
+        Adding<int>* arrAdd[n];
+        for(int i=0;i<n;i++)
+            arrAdd[i]=new Adding<int>(i-9);
+        findMin(arrAdd, n)->show();
+        }
+    break;
+    case 3:
+    {
+        Array<int> arr_i = Array<int>(8);
+        Array<double> arr_d = Array<double>(7);
+        Array<string> arr_s = Array<string>(9);
 
-    Student<string>* arrStu[2];
-    for(int i=0;i<3;i++){
-        arrStu[i]=new Student<string>(i+2,"Ala");
+        for(int i = 0; i < n; i++){
+            arr_i.add(i);
+            arr_d.add(i+0.28);
+            arr_s.add("["+to_string(i+6)+"]");
+        }
+
+        arr_i.show();   arr_d.show();   arr_s.show();
+
+        cout << arr_i.getMax()<<"\t ; \t"<<arr_d.getMax()<<"\t ; \t"<<arr_s.getMax() << endl;
+
+        arr_i.sortAsc();    arr_d.sortAsc();   arr_s.sortAsc();
+        cout << "\n\nafter sorting acs: " << endl;
+        arr_i.show();   arr_d.show();   arr_s.show();
     }
-    showAll(arrStu,3);
-
-    for(int i=0; i<3; i++)
-        delete arrAdd[i];
-
-    for(int i=0; i<2; i++)
-        delete arrStu[i];*/
-
-    /*4
-    checkType<int>(3);*/
-
-    /*zad3.2
-    Adding<int>* arrAdd[n];
-    for(int i=0;i<n;i++)
-        arrAdd[i]=new Adding<int>(i-9);
-    findMin(arrAdd, n)->show();*/
-
-    /*zad3.3 */
-    Array<int> arr_i = Array<int>(8);
-    Array<double> arr_d = Array<double>(7);
-    Array<string> arr_s = Array<string>(9);
-
-    for(int i = 0; i < n; i++){
-        arr_i.add(i);
-        arr_d.add(i+0.28);
-        arr_s.add("["+to_string(i+6)+"]");
+    break;
+    default:
+    break;
     }
-
-    arr_i.show();   arr_d.show();   arr_s.show();
-
-    cout << arr_i.getMax()<<"\t ; \t"<<arr_d.getMax()<<"\t ; \t"<<arr_s.getMax() << endl;
-
-    arr_i.sortAsc();    arr_d.sortAsc();   arr_s.sortAsc();
-    cout << "\n\nafter sorting acs: " << endl;
-    arr_i.show();   arr_d.show();   arr_s.show();
 
     return 0;
 }
