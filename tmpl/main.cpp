c852cabdc275fa27e6bb36d60849fdbffbf66146
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2024-10-09 13:10:25 +0200

    created template for main.cpp

---
+++ tmpl/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int zadN = 1;
+    printf("n?: ");
+    cin >> zadN;
+
+    switch(zadN){
+    case 1:
+    {
+    }
+    break;
+    case 2:
+    {
+    }
+    break;
+    case 3:
+    {
+    }
+    break;
+    default:
+    break;
+    }
+
+    return 0;
+}
