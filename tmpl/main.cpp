c44190cfb4ce045b8fbe0f90fe2b1f6f6ba8cc06
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2024-10-20 20:00:31 +0200

    updated template main.cpp(loop)

--- tmpl/main.cpp
+++ tmpl/main.cpp
@@ -6,24 +6,26 @@ using namespace std;
 int main()
 {
     int zadN = 1;
-    printf("n?: ");
-    cin >> zadN;
+    while(zadN != 0){
+        printf("n?: ");
+        cin >> zadN;
 
-    switch(zadN){
-    case 1:
-    {
-    }
-    break;
-    case 2:
-    {
-    }
-    break;
-    case 3:
-    {
-    }
-    break;
-    default:
-    break;
+        switch(zadN){
+        case 1:
+        {
+        }
+        break;
+        case 2:
+        {
+        }
+        break;
+        case 3:
+        {
+        }
+        break;
+        default:
+        break;
+        }
     }
 
     return 0;
